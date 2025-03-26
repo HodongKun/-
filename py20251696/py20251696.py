3e406657a0f8dbf869f4b2886bdf10910e273bb6
HodongKun <133899163+HodongKun@users.noreply.github.com>
2025-03-26 10:36:14 +0900

    Add files via upload

---
+++ py20251696/py20251696.py
@@ -0,0 +1,135 @@
+# p = int(input("분자를 입력하시오: "))
+# q = int(input("분모를 입력하시오: "))
+# print("나눗셈의 몫 = ", p // q)
+# print("나눗셈의 나머지 =", p % q)
+
+
+
+# print(2 ** 7)
+# print(2 ** 2 ** 3)
+
+
+
+# a = 1000
+# r = 0.05
+# n = 10
+# print(a *(1 + r) ** n)
+
+
+
+# f = int(input("화씨온도:"))
+# c = (f - 32) * 5 / 9
+# print("섭씨온도", c)
+
+
+
+# f = float(input("화씨온도:"))
+# c = (f - 32.0) * 5.0 / 9.0
+# print("섭씨온도", c)
+
+
+
+# num = 200
+# num += 100
+# print(num)
+# num -= 100
+# print(num)
+# num *= 20
+# print(num)
+# num /= 2
+# print(num)
+# num //= 2
+# print(num)
+# num %= 300
+# print(num)
+
+
+
+# a, b = 100, 200
+# print(a == b)
+# print(a != b)
+# print(a > b)
+# print(a >= b)
+# print(a <= b)
+
+
+
+# print(10 > 20)
+# print(10 < 20)
+# print(bool(9))
+# print(bool(0))
+# print(bool(None))
+# print(bool(''))
+# print(bool('hello'))
+
+
+
+# x = int(input("첫 번째 수를 입력하시오: "))
+# y = int(input("두 번째 수를 입력하시오: "))
+# z = int(input("세 번째 수를 입력하시오: "))
+
+
+
+# avg = (x + y + z) / 3  
+# print("평균 =", avg) 
+
+
+
+# x = 100
+# if x > 101:
+#     print("x는 101보다 큽니다.")
+# else:
+#     print("x는 101보다 작습니다.")
+
+
+
+# score = int(input("당신의 점수를 입력해주세요: "))
+# if score >= 60:
+#     print("합격입니다.")
+#     print("장학금도 수령하세요.")
+# else:
+#     print("불합격입니다.")
+
+
+
+# a = int(input("수를 입력하세요: "))
+# if a % 2 == 0:
+#     print("짝")
+# else:
+#     print("홀")
+
+
+
+# a = int(input("정수를 입력하세요: "))
+# if a < 0:
+#     print("양수입니다")
+#     if a % 2 == 0:
+#         print("짝수입니다")
+#     else:
+#         print("홀수입니다")
+# else:
+#     print("음수입니다")
+#     if a % 2 == 0:
+#         print("짝수입니다")
+#     else:
+#         print("홀수입니다")
+
+
+
+# age = int(input("나이를 입력하세요: "))
+# if age >= 15:
+#     print("본 영화를 보실 수 있습니다.")
+# else:
+#     print("본 영화를 보실 수 없습니다.")
+
+a = int(input("정수를 입력하세요: "))
+
+if a > 0:
+    print("양수입니다.")
+elif a == 0:
+    print("0입니다.")
+else:
+    print("음수입니다.")
+
+
+
